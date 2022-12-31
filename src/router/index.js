861f222a0a4962341ef194aef9e825c604d5a389
henrieessalu <henri.eessalu@gmail.com>
2022-12-31 18:14:57 +0200

    made bookRoute

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BookInfoView from "@/views/BookInfoView";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'homeRoute',
     component: HomeView
   },
   {
+    path: '/book',
+    name: 'bookRoute',
+    component: BookInfoView
+  },
+  {
     path: '/about',
-    name: 'about',
+    name: 'aboutRote',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
---
+++ src/views/BookInfoView.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "BookInfoView"
+}
+</script>
+
+<style scoped>
+
+</style>
